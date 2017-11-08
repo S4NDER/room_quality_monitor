7139ed8d10954472b1a3633ffd7af073d3ea697a
Jasper Vercnocke <jasper.vercnocke@vives.student.be>
2017-11-08 21:16:32 +0100

    Get error when login fails

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,12 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       def google_oauth2
 
         @user = User.from_omniauth(request.env['omniauth.auth'])
-
         if @user.persisted?
           sign_in_and_redirect @user, event: :authentication
         else
           session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) 
-          redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+          flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
+          redirect_to new_user_registration_url
         end
       end
       
@@ -20,17 +20,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           sign_in_and_redirect @user, :event => :authentication
         else
           session["devise.facebook_data"] = request.env["omniauth.auth"]
+          flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later.'
           redirect_to new_user_registration_url
         end
       end
 
       def github
         @user = User.from_omniauth(request.env["omniauth.auth"])
-
         if @user.persisted?
-          sign_in_and_redirect @user, :event => :authentication
+          sign_in_and_redirect @user
         else
           session["devise.github_data"] = request.env["omniauth.auth"]
+          flash[:error] = 'There was a problem signing you in through Github. Please register or try signing in later.'
           redirect_to new_user_registration_url
         end
       end
