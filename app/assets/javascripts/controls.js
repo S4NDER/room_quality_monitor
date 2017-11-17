ab9660faf71a62471729cf8367716776578da567
Jasper Vercnocke <jasper.vercnocke@vives.student.be>
2017-11-17 15:38:12 +0100

    Remove console logs

--- app/assets/javascripts/controls.js
+++ app/assets/javascripts/controls.js
@@ -4,16 +4,12 @@ $(document).ready(function() {
   $('.show_chart').click(function() {
     var device_id=$(this).data('device-id');
     $.get('/devices/'+device_id+'.json', function(data) {
-      console.log(data.device_name);
       var chart_data = {};
       
       for (var item of data.audios){
         chart_data[item.created_at]=item.value
       }
-      console.log(chart_data)
       window.chart = new Chartkick.LineChart("chart-1",chart_data)
-
-
     });
   });
 });
