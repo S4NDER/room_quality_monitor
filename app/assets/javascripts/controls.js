5e44a1f432344b3bf121c19a8be878d79bd39eba
Jasper Vercnocke <jasper.vercnocke@vives.student.be>
2017-11-18 15:00:36 +0100

    Dynamic loading charts from devices

--- app/assets/javascripts/controls.js
+++ app/assets/javascripts/controls.js
@@ -1,13 +1,45 @@
 $(document).ready(function() {
+  $('.modal').modal();
+
   $('.show_chart').click(function() {
     var device_id=$(this).data('device-id');
+    var device_type=$(this).data('device-type');
     $.get('/devices/'+device_id+'.json', function(data) {
-      var chart_data = {};
-      
-      for (var item of data.audios){
-        chart_data[item.created_at]=item.value
+      var ytitle;
+      var chart_data = {}; 
+      var device_name = data.device_name;
+
+      if (device_type == "audio"){
+        ytitle= "Audio (dB)";
+        for (var item of data.audios){
+          chart_data[item.created_at]=item.value
+        }
       }
-      window.chart = new Chartkick.LineChart("chart-1",chart_data)
+
+      else if (device_type == "temperature"){
+        ytitle= "Temperature (°C)";
+        for (var item of data.temperatures){
+          chart_data[item.created_at]=item.value
+        }
+      }
+
+      else if (device_type == "humidity"){
+        ytitle= "Humidity (%)";
+        for (var item of data.humidities){
+          chart_data[item.created_at]=item.value
+        }
+      }
+
+      else if (device_type == "luminosity"){
+        ytitle= "Luminosity (cd)";
+        for (var item of data.luminosities){
+          chart_data[item.created_at]=item.value
+        }
+      }
+      
+      document.getElementById("device_name").innerHTML = device_name;
+      window.chart = new Chartkick.LineChart("chart-1",chart_data, {xtitle: "Time", ytitle: ytitle})
     });
   });
+  
 });
--- app/views/devices/_device.json.jbuilder
+++ app/views/devices/_device.json.jbuilder
@@ -4,3 +4,18 @@ json.audios(device.audios) do |audio|
     json.value audio.value
     json.created_at audio.created_at
 end
+
+json.temperatures(device.temperatures) do |temperature|
+    json.value temperature.value
+    json.created_at temperature.created_at
+end
+
+json.luminosities(device.luminosities) do |luminosity|
+    json.value luminosity.value
+    json.created_at luminosity.created_at
+end
+
+json.humidities(device.humidities) do |humidity|
+    json.value humidity.value
+    json.created_at humidity.created_at
+end
