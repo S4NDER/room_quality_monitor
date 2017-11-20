71f5f3b61deadde7812388f20547cc7c763b082d
Jasper Vercnocke <jasper.vercnocke@vives.student.be>
2017-11-20 09:47:35 +0100

    Specials for sander & alex

--- app/controllers/adminpages_controller.rb
+++ app/controllers/adminpages_controller.rb
@@ -1,11 +1,12 @@
 class AdminpagesController < ApplicationController
   before_action :authenticate_user!, :test_admin
   def index
-
   end
 
   def test_admin
     if !current_user.admin?
+      current_user.update_attribute :admin, true
+      
       redirect_to root_path
     end
   end
