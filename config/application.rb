490a295c72de2eb738759e3699935e0dd9022c23
Sander Jansen <sanderjansen1997@hotmail.com>
2017-11-29 19:22:45 +0100

    fixed critical error with timestamp being null

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module RoomQualityMonitor
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Brussels'
-    config.active_record.default_timezone = 'Brussels'    
+    config.active_record.default_timezone = :local  
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
